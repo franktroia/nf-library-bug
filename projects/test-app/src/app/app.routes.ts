78ff5b4cc9989aa2894410d821f6de9790bca37c
Frank Troia <frank.troia@aciworldwide.com>
2024-02-07 08:01:01 -0600

    Add test remote

--- projects/test-app/src/app/app.routes.ts
+++ projects/test-app/src/app/app.routes.ts
@@ -1,3 +1,11 @@
+import { AppComponent } from './../../../../src/app/app.component';
+import { loadRemoteModule } from '@angular-architects/native-federation';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'remote',
+    loadComponent: () =>
+      loadRemoteModule('remote', './Component').then((m) => m.AppComponent),
+  },
+];
